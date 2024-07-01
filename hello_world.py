ecc5387dcd503cb8eaa59d7041ee248e21ecfa6e
GamerMANAS09 <manaslondhe21@gmail.com>
2024-07-01 08:42:09 +0000

    modified:   hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,22 @@
 message = "helloworld"
-
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# First part: constructing the string from the alphabet without delay
 for i in range(len(message)):
     for j in range(len(alphabet)):
-        prefix = alphabet[j]
-        current_str = prefix + message[:i+1]
-        if current_str == message:
-            break
+        if i == 0:  # Only print the first character of the message at position 0
+            current_str = message[:i+1]
+        else:
+            prefix = alphabet[j]
+            current_str = prefix + message[:i+1]
         print(current_str)
+        if current_str == message[:i+1]:
+            break
+
+# Second part: constructing the string from the alphabet with prefix and suffix without delay
 for k in range(len(alphabet)):
     prefix = alphabet[k]
-    current_str = prefix + message[:i+1] + prefix
-    if current_str == message:
-        break
+    current_str = prefix + message + prefix
     print(current_str)
+    if current_str == prefix + message + prefix:
+        break
